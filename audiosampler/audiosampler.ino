6c478394396932c2b46dfeafbbb790111476ef61
Cef Ramirez <cef.ramirez@gmail.com>
2017-03-27 15:39:14 -0700

    adding debug statements

--- audiosampler/audiosampler.ino
+++ audiosampler/audiosampler.ino
@@ -2,7 +2,6 @@
 
 #include <assert.h>
 #include <FreeRTOS_ARM.h>
-#include <USBCDC.h>
 #include <IPAddress.h>
 #include <PowerDueWiFi.h>
 
@@ -11,22 +10,33 @@
 #include "ADCTrigger.h"
 #include "ADCClock.h"
 #include "PowerDue.h"
+
+#include "debug.h"
 #include "packet.h"
 #include "synchronization.h"
 
 QueueHandle_t xBufferQueue;
-QueueHandle_t xSerialQueue;
 QueueHandle_t xTcpQueue;
 ADCTrigger trigger;
 
 // store the timestamps for each buffer to report later
 tstamp_t tstamps[NUM_BUFFERS];
 
-#define PACKET_BUFFER 2
+#define PACKET_BUFFER_COUNT 2   // packet buffers
 
-AcousticEvent_t capturedEvent[PACKET_BUFFER];
-void initPacket(){
-  for(int p=0; p < PACKET_BUFFER; p++){
+AcousticEvent_t capturedEvent[PACKET_BUFFER_COUNT];
+
+/*
+ *  Initialization Routines
+ */
+
+inline void initQueues(){
+  xBufferQueue = xQueueCreate(BUFFER_QUEUE_LENGTH, sizeof(uint8_t));
+  xTcpQueue = xQueueCreate(BUFFER_QUEUE_LENGTH, sizeof(uint8_t));
+}
+
+inline void initPacket(){
+  for(int p=0; p < PACKET_BUFFER_COUNT; p++){
     capturedEvent[p].pktHeader = EVENT_PACKET_HEADER;
     for(int i=0; i < DEVICE_ID_LENGTH; i++){
       capturedEvent[p].deviceID[i] = DEVICE_ID[i];
@@ -36,9 +46,7 @@ void initPacket(){
     }
     capturedEvent[p].samplingFrequency = ADC_SAMPLE_RATE;  
     capturedEvent[p].pktFooter = EVENT_PACKET_FOOTER;
-  }
-  
-  SerialUSB2.println("Packet initialized");
+  }  
 }
 
 inline void initPins(){
@@ -51,23 +59,7 @@ inline void togglePin(int state){
   pinState = state;
 }
 
-void adc_full(uint8_t index, uint16_t *readyBuffer, uint16_t size){
-  // clock goes tick!
-  ADCClock.tick();
-  
-  // get current time!
-  tstamps[index] = ADCClock.getTime();
-  
-  // if even, toggle low
-  // if odd, toggle high
-  togglePin(ADCClock.getMajorTicks()%2);
-  xQueueSendToBackFromISR(xBufferQueue, &index, NULL);
-}
-
-/**
- *  Handles each buffer received
- */
-void BufferHandlerTask(void *arg){
+inline void initADC(){
   ADCClock.init(BUFFER_SIZE);
   ADCClock.reset();
   
@@ -77,30 +69,58 @@ void BufferHandlerTask(void *arg){
   ADCSampler.setInterruptCallback(adc_full);
   ADCSampler.reset();
   ADCSampler.start();
+}
+
+
+
+void adc_full(uint8_t index, uint16_t *readyBuffer, uint16_t size){
+  // clock goes tick!
+  ADCClock.tick();
+  
+  // save current time!
+  tstamps[index] = ADCClock.getTime();
+  
+  // if even, toggle low... if odd, toggle high
+  togglePin(ADCClock.getMajorTicks()%2);
+  
+  // send buffer index for processing
+  xQueueSendToBackFromISR(xBufferQueue, &index, NULL);
+}
+
+/**
+ *  Handles each buffer received
+ */
+void BufferHandlerTask(void *arg){
+  initADC();
   uint8_t bufIndex;
   uint8_t bufCount = 0;
   uint8_t currPacket = 0;
-  SerialUSB2.println("Sampling start!");
   while(1){
     bufIndex = 0;
     if(xQueueReceive(xBufferQueue, &bufIndex, portMAX_DELAY)){
+      
+      // process the buffer to check for an event
       trigger.feed(ADCSampler.getBufferAtIndex(bufIndex), BUFFER_SIZE);
+      
+      // did we detect an event? Should we queue up buffers?
       if(bufCount > 0 || trigger.isTriggered()){
         trigger.reset();
-        // we want to output 8 buffers
+        // we want to send BUFFERS_TO_SEND buffers
+        // if this is the first time to trigger, set how many buffers we should count
         if(bufCount == 0){
           pd_rgb_led(PD_BLUE);
           bufCount = BUFFERS_TO_SEND; // start sending out buffers
           
+          // start by sending the previous buffer
           uint8_t ind = 0;
           if(bufIndex == 0){
             ind = NUM_BUFFERS-1;
           } else {
             ind = bufIndex-1;
           }
-          
-          // start by sending the previous 1 buffers
+
           capturedEvent[currPacket].timestamp = tstamps[ind];
+          // capture the previous buffer into the packet
           memcpy( 
                   &(capturedEvent[currPacket].samples[BUFFER_SIZE * (BUFFERS_TO_SEND-bufCount)]), 
                   (uint8_t *)ADCSampler.getBufferAtIndex(ind),
@@ -108,20 +128,21 @@ void BufferHandlerTask(void *arg){
                 );
           bufCount--;
         }
-        // for now, do the dumb thing of sending it all through the serial port
-        // the samples are in uint16_t format, we can only write by uint8_t formats
-        // reinterpret as a byte buffer and specify size in bytes       
+        
+        // copy the current buffer into the packet
         memcpy( 
                 &(capturedEvent[currPacket].samples[BUFFER_SIZE * (BUFFERS_TO_SEND-bufCount)]), 
                 (uint8_t *)ADCSampler.getBufferAtIndex(bufIndex),
                 BUFFER_SIZE * BYTES_PER_SAMPLE
               );
         bufCount--;
+        
+        // when we've collected BUFFERS_TO_SEND buffers, queue the packet for transmission
         if(bufCount == 0){
           // send out the buffer
           sendPacket(currPacket);
-          currPacket = (currPacket+1)%PACKET_BUFFER;  
-          pd_rgb_led(PD_RED);        
+          currPacket = (currPacket+1)%PACKET_BUFFER_COUNT;  
+          pd_rgb_led(PD_RED);
         }
       }
     }
@@ -129,21 +150,9 @@ void BufferHandlerTask(void *arg){
   ADCSampler.stop();
 }
 
-void sendPacket(uint8_t bufIndex){
+void sendPacket(uint8_t packetIndex){
   // don't block when sending
-  xQueueSendToBack(xSerialQueue, &bufIndex, 0);
-  xQueueSendToBack(xTcpQueue, &bufIndex, 0);
-}
-
-void SerialStreamOut(void *arg){
-  SerialUSB.begin(0);
-  uint8_t bufIndex;
-  while(1){
-    if(xQueueReceive(xSerialQueue, &bufIndex, portMAX_DELAY)){
-      uint8_t *buf = (uint8_t *)(&capturedEvent[bufIndex]);
-      SerialUSB.write(buf, PACKET_LENGTH);
-    }
-  }
+  xQueueSendToBack(xTcpQueue, &packetIndex, 0);
 }
 
 void TcpStreamOut(void *arg){
@@ -161,9 +170,9 @@ void TcpStreamOut(void *arg){
     int s = lwip_socket(AF_INET, SOCK_STREAM, 0);
     int r = 0;
     while(r = lwip_connect(s, (struct sockaddr *)&serverAddr, sizeof(serverAddr))){
-      SerialUSB2.println("Failed to connect to server");
-      SerialUSB2.print("Error code: ");
-      SerialUSB2.println(r);
+      PRINT_DEBUGLN("Failed to connect to server!!!");
+      
+      // retry
       lwip_close(s);
       lwip_shutdown(s, SHUT_RDWR);
       // flush the receive queue so we don't block the rest of the system
@@ -172,16 +181,15 @@ void TcpStreamOut(void *arg){
       vTaskDelay(pdMS_TO_TICKS(2000)); // wait a bit and try again
     }
     
-    uint8_t bufIndex;
+    uint8_t packetIndex;
     
     // inner loop to handle sending data
     while(1){
-      if(xQueueReceive(xTcpQueue, &bufIndex, portMAX_DELAY)){
+      if(xQueueReceive(xTcpQueue, &packetIndex, portMAX_DELAY)){
         pd_rgb_led(PD_GREEN);
-        if(lwip_write(s, (uint8_t *)&capturedEvent[bufIndex], PACKET_LENGTH) < 0){
+        if(lwip_write(s, (uint8_t *)&capturedEvent[packetIndex], PACKET_LENGTH) < 0){
           // an error occurred during write.
           // did we disconnect? break out and try to reconnect.
-          SerialUSB2.println("Failed to send. Trying to reconnect.");
           break;
         }
         pd_rgb_led(PD_RED);
@@ -191,54 +199,57 @@ void TcpStreamOut(void *arg){
     // close socket after everything is done
     lwip_close(s);
     lwip_shutdown(s, SHUT_RDWR);
-    SerialUSB.println("socket closed");
   }
   
 }
 
 void onError(int errorCode){
-  SerialUSB2.print("Error received: ");
-  SerialUSB2.println(errorCode);
+  // An error occurred when trying to connect to the network
+  PRINT_DEBUG("Failed to initialize device: ");
+  PRINT_DEBUGLN(errorCode);
+  
+  assert(false);
 }
 
 void onReady(){
-  SerialUSB2.println("Device ready");  
-  SerialUSB2.print("Device IP: ");
-  SerialUSB2.println(IPAddress(PowerDueWiFi.getDeviceIP()));  
-
+  // WiFi connection ready!
+  // initialize tasks
+  PRINT_DEBUGLN("Device Ready!");
+  PRINT_DEBUG("Device IP: ");
+  PRINT_DEBUGLN(IPAddress(PowerDueWiFi.getDeviceIP()));
+  
   startTasks();
 }
 
 void startTasks(void){
   xTaskCreate(BufferHandlerTask, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-  xTaskCreate(SerialStreamOut, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
   xTaskCreate(TcpStreamOut, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-  startSyncTasks(2);
+  startSyncTasks(NTP_TASK_PRIORITY);
 }
 
 void setup(){
+  
+#if DEBUG
+  // only needed when debug is turned on
+  // most likely not needed when already deployed
   SerialUSB.begin(0);
-  SerialUSB2.begin(0);
-  while(!SerialUSB2);
+  while(!SerialUSB);
+#endif
   
   initPins();
   initPacket();
+  initQueues();
   pd_rgb_led_init();
   
-  xBufferQueue = xQueueCreate(BUFFER_QUEUE_LENGTH, sizeof(uint8_t));
-  xSerialQueue = xQueueCreate(BUFFER_QUEUE_LENGTH, sizeof(uint8_t));
-  xTcpQueue = xQueueCreate(BUFFER_QUEUE_LENGTH, sizeof(uint8_t));
-  
+  // initialize WiFi connection and lwIP stack
   PowerDueWiFi.init(WIFI_SSID, WIFI_PASS);
   PowerDueWiFi.setCallbacks(onReady, onError);
 
-  // startTasks();
-
   vTaskStartScheduler();
-  SerialUSB2.println("Insufficient memory");
+
   while(1);
 }
 
 void loop(){
-  // do nothing
+  // sleep...
 }
