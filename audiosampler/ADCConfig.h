70a549abfd912f9057ba77c8ffbfd8450cc9672a
Cef Ramirez <cef.ramirez@gmail.com>
2017-03-27 15:04:31 -0700

    labeling parameters

--- audiosampler/ADCConfig.h
+++ audiosampler/ADCConfig.h
@@ -7,64 +7,68 @@
  * Device Parameters
  */
 
-#define DEVICE_ID "PDue0002"    // use this to uniquely identify your device
+#define DEVICE_ID "PDue0000"    // use this to uniquely identify your device (max of 8 characters)
 
 /*
  * Application Parameters
  */
 
-// update these
-// #define WIFI_SSID "PowerDue"
-// #define WIFI_PASS "powerdue"
-// #define SERVER_IP "10.230.12.1"
-
-#define WIFI_SSID "YouGotMe"
-#define WIFI_PASS "y0ug0tmeg00d"
-#define SERVER_IP "192.168.1.9"
-
-#define CLOCK_GPIO_OUT 51
-#define BUFFER_QUEUE_LENGTH 8
-#define BUFFERS_TO_SEND 8
+// TODO: Update these values to the proper network / server IP
+#define WIFI_SSID "PowerDue"
+#define WIFI_PASS "powerdue"
+#define SERVER_IP ""  // typically the IP of your Laptop (e.g. "10.230.12.1")
 
 /*
  * Clock parameters
  */
 
-/* defines the size of a timestamp that's sent over the network */
-typedef uint64_t tstamp_t;
-
 // This defines whether this device is the master clock or not
-// 0 - this is a slave clock
+// 0 - this is a slave clock (see below for extra parameters)
 // 1 - this is the master clock
 #define MASTER_CLOCK 0
 
 // The udp port to use for synchronization
-#define SYNC_PORT 12345
+#define SYNC_PORT 12345         // no need to modify. must be consistent across all devices
 
 #if MASTER_CLOCK == 0
-  // If this is a slave clock, this must be defined properly
-  // #define MASTER_CLOCK_IP "10.230.12.8"  // e.g. "10.230.12.10" ?
-  #define MASTER_CLOCK_IP "192.168.1.15"  // e.g. "10.230.12.10" ?
+  // TODO: If this is a slave clock, this must be defined properly
+  // Set this to the IP Address of your Master Clock
+  #define MASTER_CLOCK_IP ""  // e.g. "10.230.12.10"
 #endif
 
-// Time period between synchronization trials (in milliseconds)
-#define SYNC_FREQUENCY  2000
+// Time period between NTP synchronization trials (in milliseconds)
+#define SYNC_FREQUENCY  10000
 
 /*
  *  ADCSampler parameters
  */
+ 
+#define ADC_SAMPLE_RATE 1000    // Sampling Rate in Hz of the ADC Sampler
 
-#define NUM_BUFFERS   16
-#define BUFFER_SIZE   128
-#define BYTES_PER_SAMPLE 2
+#define NUM_BUFFERS   8         // How many buffers should the ADC Sampler keep track of?
 
-#define ADC_CHANNEL_MIC 2       // mic is connected to A2
-#define ADC_SAMPLE_RATE 50000
+#define BUFFER_SIZE   256       // How many samples per buffer should the DMA fill up
+
+#define BUFFERS_TO_SEND 4       // How many buffers to send to the visualizer
 
 /*
  * ADCTrigger parameters
  */
-#define WINDOW_COUNT 8
-#define MIN_WINDOW_COUNT 24
+ 
+#define WINDOW_COUNT 8          // How many signal windows to keep track of
+#define MIN_WINDOW_COUNT 24     // How many signal windows to ignore on start up before enabling triggering
 
+/*
+ * Do not modify the following parameters
+ */ 
+#define ADC_CHANNEL_MIC 2       // mic is connected to A2
+
+#define BYTES_PER_SAMPLE 2      // the ADC generates 12-bit samples (i.e. 2 bytes of data per sample)
+ 
+typedef uint64_t tstamp_t;      /* defines the size of a timestamp that's sent over the network */
+
+#define CLOCK_GPIO_OUT 51       // GPIO pin to toggle on each clock tick
+
+#define BUFFER_QUEUE_LENGTH 8   // FreeRTOS Queue length 
+ 
 #endif  //ADCCONFIG_H
